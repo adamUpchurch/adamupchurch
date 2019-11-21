0fb54f42cdd2a874faba418a902b950666d9f1ba
adam@ausolution.com <adam@adams-mbp.attlocal.net>
2019-11-20 20:32:31 -0500

    fixed logo

--- src/components/_ui/Logo.jsx
+++ src/components/_ui/Logo.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Logod = styled("div")`
+    font-family: 'Zilla Slab', serif;
+    font-size: 2rem;
+    color: black;
+    text-decoration-line: none;
+
+`
 
 const Logo = () => (
-    <img src="https://cdn.cp.adobe.io/content/2/dcx/284754c6-db00-40e2-82f0-047b1328285f/rendition/preview.jpg/version/2/format/jpg/dimension/width/size/300" alt="Adam Upchurch"/>
+    <Logod>
+        <text>Adam.</text>
+    </Logod>
 );
 
 export default Logo;
